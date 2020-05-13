ea206da3c2c964a668902f4634ae98d2838b826a
Crowspeak <afcajamarcar@unal.edu.co>
2020-05-13 02:14:13 -0500

    added mongodb and mongoose to proyect, implemented connection and model, dockerization of mongodb

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 import express from 'express';
+import * as db from './db';
+import { Thing } from './models/Thing';
 
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {res.send('Hello world!')})
 
+(async () => { await db.initMongo() })();
+
+app.get('/', (req, res) => { res.send('Hello world!') })
+
+app.get('/getThing', async (req, res) => {
+    try {
+        let result = await Thing.find({});
+        console.log(JSON.stringify(result));
+
+        res.send(result);
+
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 app.listen(port, function () {
-  console.log(`Things running on port ${port}!`);
+    console.log(`Things running on port ${port}!`);
 });
